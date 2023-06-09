d046ebb4d1b4337d7cce1e151d38ae3d244a4902
cloacr <136034424+cloacr@users.noreply.github.com>
2023-06-08 18:22:30 -0600

    Add files via upload

---
+++ Hellow.py
@@ -0,0 +1,20 @@
+"""
+Your module description
+"""
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+
+print(myFruitList[0])
+
+print(myFruitList[1])
+
+print(myFruitList[2])
+
+myFruitList[2] = "orange"
+
+print(myFruitList)
+
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
